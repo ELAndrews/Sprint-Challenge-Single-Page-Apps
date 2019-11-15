eb5f0f519b441542e0abd465410d205bd288401c
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-15 08:25:11 +0000

    Add CharacterCard return skeleton, displayed API data

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+  return (
+    <div>
+      <h3>{props.curr.name}</h3>
+      <img src={props.curr.image} alt={props.curr.name} />
+      <p>{props.curr.species}</p>
+      <p>{props.curr.origin.name}</p>
+      <p>{props.curr.species}</p>
+    </div>
+  )
 }
